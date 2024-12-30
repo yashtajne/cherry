3ad38d3a185657083f719c5197f9e5560be23c3e
yashtajne <tajneyashraj15@gmail.com>
2024-12-30 17:47:50 +0530

    refactor: better version command

--- cmds/version.go
+++ cmds/version.go
@@ -2,27 +2,11 @@ package cmds
 
 import (
 	"fmt"
-
-	. "github.com/yashtajne/cherry/utils"
 )
 
 func Version(version string) {
-
-	// gcc version
-	gcc_version, err := GetCompilerVersion("gcc")
-	if err != nil {
-		fmt.Printf("Error (getting gcc version): %v", err)
-		return
-	}
-
-	// g++ version
-	gpp_version, err := GetCompilerVersion("g++")
-	if err != nil {
-		fmt.Printf("Error (getting g++ version): %v", err)
-		return
-	}
-
-	fmt.Println("version: ", version)
-	fmt.Println(gcc_version)
-	fmt.Println(gpp_version)
+	fmt.Println("Cherry ", version)
+	fmt.Println("Copyright (C) 2024 Yash Tajne")
+	fmt.Println("This program may be freely redistributed")
+	fmt.Println("under the terms of the MIT License.")
 }
